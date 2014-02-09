cab1a481ecbd2abec89a94b45291e4e91e496455
Yuval Dekel <thedekel@gmail.com>
2014-02-09 11:50:57 -0500

    sending basic files with basic styling

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req, res) {
-  res.end('hello world');
+  res.sendfile('./res/chat.html');
+});
+
+app.get('/style.css', function(req, res) {
+  res.sendfile('./res/style.css');
+});
+
+app.get('/script.js', function(req, res) {
+  res.sendfile('./res/script.js');
 });
 
 app.listen(8000);
